6bff373ceab4aa02ef2c07ba4966cf54c831cd00
Nadeem <muhammad.nadeem1@non.se.com>
2023-12-16 08:52:02 +0000

    fixed the hilt dependency

--- domain/src/main/java/com/logical/domain/usecase/GetCountriesUseCase.kt
+++ domain/src/main/java/com/logical/domain/usecase/GetCountriesUseCase.kt
@@ -1,13 +1,15 @@
 package com.logical.domain.usecase
 
 import android.content.res.Resources
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import com.logical.domain.R
 import com.logical.domain.common.Resource
 import com.logical.domain.model.CountryItemModel
 import com.logical.domain.repository.CountriesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
 import java.io.IOException
 
 class GetCountriesUseCase(
@@ -16,6 +18,7 @@ class GetCountriesUseCase(
 ) {
 
 
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     operator fun invoke(): Flow<Resource<List<CountryItemModel>>> =
         flow {
             try {
